57a0d6f8b1b110351d42b4e236bc99919bb93861
Andrew T. Baker <andrew.tork.baker@gmail.com>
2014-10-22 16:52:23 -0400

    Closer to MVP

--- twitter-approver/twitter/models.py
+++ twitter-approver/twitter/models.py
@@ -1,4 +1,8 @@
+from django.conf import settings
 from django.db import models
+from requests_oauthlib import OAuth1
+
+import requests
 
 class Tweet(models.Model):
     body = models.CharField(max_length=250)
@@ -10,3 +14,22 @@ class Tweet(models.Model):
 
     def __unicode__(self):
         return u'{0}'.format(self.body)
+
+    def save(self, *args, **kwargs):
+        if self.pk is not None:
+            original = Tweet.objects.get(pk=self.pk)
+            if (not original.approved) and self.approved:
+                self.publish()
+        super(Tweet, self).save(*args, **kwargs)
+
+    def publish(self):
+        url = 'https://api.twitter.com/1.1/statuses/update.json'
+        auth = OAuth1(settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET,
+                  settings.ATB_ACCESS_TOKEN, settings.ATB_TOKEN_SECRET)
+        payload = {
+            'status': self.body,
+        }
+
+        # TO-DO: Add some error handling here
+        requests.post(url, auth=auth, params=payload)
+        return
--- twitter-approver/twitter/views.py
+++ twitter-approver/twitter/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from rest_framework import viewsets
 
 from .models import Tweet
@@ -11,15 +10,3 @@ class TweetViewSet(viewsets.ModelViewSet):
     """
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializer
-
-def get_my_tweets(request):
-    import requests
-    from requests_oauthlib import OAuth1
-
-    url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
-    auth = OAuth1(settings.TWITTER_API_KEY, settings.TWITTER_API_SECRET,
-                  settings.ATB_ACCESS_TOKEN, settings.ATB_TOKEN_SECRET)
-    response = requests.get(url, auth=auth)
-    import pdb; pdb.set_trace();
-
-
