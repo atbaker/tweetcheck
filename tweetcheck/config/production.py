35965dba496e9a220decbdd8d5f3267667ab0d3a
Andrew T. Baker <andrew.tork.baker@gmail.com>
2014-12-23 21:42:01 -0500

    Tightening up prod

--- tweetcheck/config/production.py
+++ tweetcheck/config/production.py
@@ -8,7 +8,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 ALLOWED_HOSTS = ['.tweetcheck.com']
 
-# REST Framework (remove SessionAuthentication)
+# REST Framework
 REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
   'rest_framework.authentication.TokenAuthentication',
+)
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
+    'rest_framework.renderers.JSONRenderer',
 )
