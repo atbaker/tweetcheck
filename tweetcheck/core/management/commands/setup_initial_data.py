bed013ef1bf53c61d15aca6d0281e74cca1f8c14
Andrew T. Baker <andrew.tork.baker@gmail.com>
2015-04-30 17:54:12 -0400

    Removing my personal email address from the admin user

--- tweetcheck/core/management/commands/setup_initial_data.py
+++ tweetcheck/core/management/commands/setup_initial_data.py
@@ -14,10 +14,10 @@ class Command(BaseCommand):
 
         # Create admin superuser
         try:
-            TweetCheckUser.objects.get(email='andrew.tork.baker@gmail.com')
+            TweetCheckUser.objects.get(email='admin@tweetcheck.com')
         except TweetCheckUser.DoesNotExist:
-            TweetCheckUser.objects.create_superuser(email='andrew.tork.baker@gmail.com',
-                password='g',
+            TweetCheckUser.objects.create_superuser(email='admin@tweetcheck.com',
+                password='admin',
                 organization=org)
 
         # Create scheduler user
@@ -25,14 +25,14 @@ class Command(BaseCommand):
             TweetCheckUser.objects.get(email='scheduler@tweetcheck.com')
         except TweetCheckUser.DoesNotExist:
             TweetCheckUser.objects.create_superuser(email='scheduler@tweetcheck.com',
-                password='s')
+                password='scheduler')
 
         # Create test nonapprover
         try:
             TweetCheckUser.objects.get(email='nonapprover@tweetcheck.com')
         except TweetCheckUser.DoesNotExist:
             TweetCheckUser.objects.create_user(email='nonapprover@tweetcheck.com',
-                password='n',
+                password='nonapprover',
                 is_approver=False,
                 organization=org)
 
